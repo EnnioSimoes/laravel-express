ac1561163e59652bbf15fc084622bf6a39d12e39
Ennio Simões <ennio.simoes@outlook.com>
2015-09-11 20:10:55 -0300

    tags add

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -6,7 +6,14 @@
     @foreach($posts as $post)
         <h2>{{ $post->title }}</h2>
         <p>{{ $post->content }}</p>
+
+        <h4>Comentários</h4>
+        @foreach($post->comments as $comment)
+            <b>Name: </b> {{$comment->name}}
+            <b>Comentário: </b> {{$comment->comment}}
+        @endforeach
         <hr />
     @endforeach
+
 
 @endsection
